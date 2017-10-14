01b8e291b6283f92af2b4804750c9f9d23f56cb0
koryd <korydonati@gmail.com>
2017-10-13 23:24:13 -0700

    finished leap, started next thing

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,8 @@
 def is_leap_year(year):
-    pass
+    four = year % 4
+    hund = year % 100
+    four_hund = year % 400
+
+    if four == 0 and (four_hund == 0 or hund != 0):
+        return True
+    return False
