6350e90e85c67f4d411f57f5313f705f6626c4a6
koryd <korydonati@gmail.com>
2017-10-13 23:37:13 -0700

    solved isogram, on to next

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,9 @@
 def is_isogram(string):
-    pass
+    import re
+
+    string = re.sub(r'[^a-z]', '', string.lower())
+    str_set = set(string)
+
+    if len(str_set) == len(string):
+        return True
+    return False
